2a67c897dfed05c7f067043fe52a074aabb0bbe6
Mrdimss <fasadimas.76.243@gmail.com>
2025-01-08 12:36:04 +0700

    test

---
+++ src/app/api/uploadthing/route.ts
@@ -0,0 +1,15 @@
+import { createRouteHandler } from "uploadthing/next";
+import { ourFileRouter } from "./core";
+import { UTApi } from "uploadthing/server";
+
+export const { GET, POST } = createRouteHandler({
+  router: ourFileRouter,
+});
+
+export async function DELETE(request: Request) {
+  const { fileKey } = await request.json();
+  const utApi = new UTApi();
+  await utApi.deleteFiles(fileKey);
+
+  return Response.json({ message: "Image Deleted" })
+};
